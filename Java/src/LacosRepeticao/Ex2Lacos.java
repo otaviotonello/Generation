2230b6d4a0ca755a34039a4c5fa425ab690339b2
Otavio Tonello <otaviotonello0102@gmail.com>
2021-11-10 10:18:07 -0300

    Update Exercicios Java

--- Java/src/LacosRepeticao/Ex2Lacos.java
+++ Java/src/LacosRepeticao/Ex2Lacos.java
@@ -20,15 +20,15 @@ public class Ex2Lacos {
 		System.out.print("Digite outro número: ");
 		c = in.nextDouble();
 		
-		if(a>b && b>c) {
+		if(a<b && b<c) {
 			System.out.println("Ordem crescente: " + a + " " + b + " " + c);
-		} else if(a>b && c>b) {
+		} else if(a<b && c<b) {
 			System.out.println("Ordem crescente: " + a + " " + c + " " + b);
-		} else if(b>a && a>c) {
+		} else if(b<a && a<c) {
 			System.out.println("Ordem crescente: " + b + " " + a + " " + c);
-		} else if(b>a && c>a) {
+		} else if(b<a && c<a) {
 			System.out.println("Ordem crescente: " + b + " " + c + " " + a);
-		} else if(c>b && b>a) {
+		} else if(c<b && b<a) {
 			System.out.println("Ordem crescente: " + c + " " + b + " " + a);
 		} else {
 			System.out.println("Ordem crescente: " + c + " " + a + " " + b);
